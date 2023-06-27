f2a81433ce45e3963364bfbccfef4d19fa610149
Antonio Saracino <antonio.saracino83@gmail.com>
2023-06-27 03:54:48 +0200

    Added divider, texts and computer player

--- app/src/main/java/com/example/dicegamecompose/MainActivity.kt
+++ app/src/main/java/com/example/dicegamecompose/MainActivity.kt
@@ -5,15 +5,25 @@ import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxHeight
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.material3.Divider
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
+import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import com.example.dicegamecompose.ui.theme.DiceGameComposeTheme
 import kotlin.random.Random
 
@@ -21,24 +31,54 @@ class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
-            DiceGameComposeTheme {
-                // A surface container using the 'background' color from the theme
-                Surface(
-                    modifier = Modifier.fillMaxSize(),
-                    color = MaterialTheme.colorScheme.background
-                ) {
-                    Column {
-                        val diceValue = remember { mutableStateOf(1) }
-                        DieCast(diceValue.value, onClick = { diceValue.value = rollDice() })
-
-                    }
-
-                }
-            }
+            MainContent()
         }
     }
 }
 
+@Composable
+fun MainContent() {
+    DiceGameComposeTheme {
+        Surface(
+            modifier = Modifier.fillMaxSize(),
+            color = MaterialTheme.colorScheme.background
+        ) {
+            Column(
+                modifier = Modifier.fillMaxHeight(),
+                verticalArrangement = Arrangement.Center,
+                horizontalAlignment = Alignment.CenterHorizontally
+            ) {
+                val diceValue1 = remember { mutableStateOf(6) }
+                val diceValue2 = remember { mutableStateOf(6)}
+                Text("Player", modifier = Modifier.padding(10.dp))
+                DieCast(
+                    diceValue1.value,
+                    onClick = { diceValue1.value = rollDice() }
+                )
+                Divider(
+                    modifier = Modifier.padding(start = 30.dp, end = 30.dp),
+                    thickness = 1.dp,
+                    color = Color.Black
+                )
+                Text("Computer", modifier = Modifier.padding(10.dp))
+                DieCast(
+                    diceValue2.value,
+                    onClick = { diceValue2.value = rollDice() }
+                )
+
+            }
+
+        }
+    }
+}
+
+@Preview(showBackground = true)
+@Composable
+fun MainContentPreview() {
+    DiceGameComposeTheme {
+        MainContent()
+    }
+}
 
 @Composable
 fun DieCast(n: Int, onClick: () -> Unit) {
@@ -56,8 +96,8 @@ fun DieCast(n: Int, onClick: () -> Unit) {
         painter = painterResource(id = drawableRes),
         contentDescription = "Dice Game!",
         modifier = Modifier
-            .clickable { onClick()}
-            .fillMaxSize()
+            .clickable { onClick() }
+            .size(300.dp)
     )
 }
 
